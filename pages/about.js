157333a0b05cdfadda06ae12631e30bc82432696
Parvez.Shaikh <parvez.shaikh@bookmyshow.com>
2019-07-21 13:51:54 +0530

    Test content update

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import Link from 'next/link'
+import Head from 'next/head'
 
 const About = () => (
-    <div>
-        <Link href='/'><a>Home</a></Link>
-        <Link href='/pages'><a>Pages</a></Link>
-        <Link href='/about'><a>About</a></Link>
-        <h1>ABOUT PAGE</h1>
-        <p>THIS IS ABOUT PAGE</p>
-    </div>
+	<div>
+		<Head>
+            <meta name="description" content="this is my description" />
+            <meta name="keywords" content="React boilerberg" />
+            <meta content="IE=edge" httpEquiv="" />
+            <meta content="yes" name="mobile-web-app-capable" />
+            <title>About PAGE</title>
+        </Head>
+		<Link href='/'><a>Home</a></Link>
+		<Link href='/pages'><a>Pages</a></Link>
+		<Link href='/about'><a>About</a></Link>
+		<h1>ABOUT PAGE</h1>
+		<p>THIS IS ABOUT PAGE</p>
+	</div>
 )
 
 export default About
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from "react"
 import Link from 'next/link'
+import Head from 'next/head'
 
 const Index = () => (
     <div>
+        <Head>
+            <meta name="description" content="this is my description" />
+            <meta name="keywords" content="React boilerberg" />
+            <meta content="IE=edge" httpEquiv="" />
+            <meta content="yes" name="mobile-web-app-capable" />
+            <title>HOME PAGE</title>
+        </Head>
         <Link href='/'><a>Home</a></Link>
         <Link href='/pages'><a>Pages</a></Link>
         <Link href='/about'><a>About</a></Link>
