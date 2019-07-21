0bd07de1c5a8ab42430f74b25c196eeb697dc952
Parvez.Shaikh <parvez.shaikh@bookmyshow.com>
2019-07-21 13:52:17 +0530

    Added Custom Document Page

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Document, { Html, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang='en'>
+                <body>
+                    <Main />
+					<NextScript />
+				</body>
+            </Html>
+        )
+    }
+}
